f064285712ae9234994d63fb2e911c34f0ea5d9f
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-09-10 19:31:10 -0600

    feat: github auth provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,11 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
 import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
 import { type Adapter } from "next-auth/adapters";
-import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -48,10 +48,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    })
     /**
      * ...add more providers here.
      *
