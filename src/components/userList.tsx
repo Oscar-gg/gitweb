e681ec098735d3bfae8e035ee82b56b3a7038e1b
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-09-12 22:12:21 -0600

    delete dark theme

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -21,23 +21,23 @@ const UserCard = ({ id }: { id: string }) => {
   const { data: user } = api.user.getUserById.useQuery({ id });
 
   return (
-    <div className="bg-gray-200 pt-12 dark:bg-gray-700">
-      <div className="mx-auto max-w-sm overflow-hidden rounded-lg bg-white shadow-lg dark:bg-gray-900">
+    <div className="bg-gray-200 pt-12">
+      <div className="mx-auto max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ">
         <div className="border-b px-4 pb-6">
           <div className="my-4 text-center">
             <img
-              className="mx-auto my-4 h-32 w-32 rounded-full border-4 border-white dark:border-gray-800"
+              className="mx-auto my-4 h-32 w-32 rounded-full border-4 border-white"
               src={user?.image ?? env.NEXT_PUBLIC_DEFAULT_PROFILE_IMAGE}
-              alt="{Profile picture}"
+              alt="Profile picture"
               
             />
             <div className="py-2">
-              <h3 className="mb-1 text-2xl font-bold text-gray-800 dark:text-white">
+              <h3 className="mb-1 text-2xl font-bold text-gray-800 ">
                 {user?.name}
               </h3>
-              <div className="inline-flex items-center text-gray-700 dark:text-gray-300">
+              <div className="inline-flex items-center text-gray-700 ">
                 <svg
-                  className="mr-1 h-5 w-5 text-gray-400 dark:text-gray-600"
+                  className="mr-1 h-5 w-5 text-gray-400 "
                   fill="currentColor"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -58,15 +58,15 @@ const UserCard = ({ id }: { id: string }) => {
           </div>
 
           <div className="flex gap-2 px-2">
-            <button className="flex-1 rounded-full bg-blue-600 px-4 py-2 font-bold text-white antialiased hover:bg-blue-800 dark:bg-blue-800 dark:text-white dark:hover:bg-blue-900">
+            <button className="flex-1 rounded-full bg-blue-600 px-4 py-2 font-bold text-white antialiased hover:bg-blue-800">
               View Repositories
             </button>
           </div>
         </div>
         <div className="px-4 py-4">
-          <div className="mb-4 flex items-center gap-2 text-gray-800 dark:text-gray-300">
+          <div className="mb-4 flex items-center gap-2 text-gray-800 ">
             <svg
-              className="h-6 w-6 text-gray-600 dark:text-gray-400"
+              className="h-6 w-6 text-gray-600 "
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -79,7 +79,7 @@ const UserCard = ({ id }: { id: string }) => {
               />
             </svg>
             <span>
-              <strong className="text-black dark:text-white">
+              <strong className="text-black ">
                 {user?.followers}
               </strong>{" "}
               Followers
@@ -88,36 +88,36 @@ const UserCard = ({ id }: { id: string }) => {
           <div className="flex">
             <div className="mr-2 flex justify-end">
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/men/32.jpg"
                 alt=""
               />
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/women/31.jpg"
                 alt=""
               />
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/men/33.jpg"
                 alt=""
               />
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/women/32.jpg"
                 alt=""
               />
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/men/44.jpg"
                 alt=""
               />
               <img
-                className="-mr-2 h-10 w-10 rounded-full border-2 border-white dark:border-gray-800"
+                className="-mr-2 h-10 w-10 rounded-full border-2 border-white "
                 src="https://randomuser.me/api/portraits/women/42.jpg"
                 alt=""
               />
-              <span className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-gray-200 bg-white text-sm font-semibold text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-white">
+              <span className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-gray-200 bg-white text-sm font-semibold text-gray-800 ">
                 +999
               </span>
             </div>
